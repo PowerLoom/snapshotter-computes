b744009745a819ce12f13d4127d68900edd7b4a0
Seth <seth@powerloom.io>
2024-03-26 00:07:39 -0400

    chore: update initial pairs and metadata for new token list

--- tests/token_derived_eth_test.py
+++ tests/token_derived_eth_test.py
@@ -1,4 +1,5 @@
 import asyncio
+from pprint import pprint
 
 from snapshotter.utils.rpc import RpcHelper
 from snapshotter.utils.default_logger import logger
@@ -10,19 +11,19 @@ from ..utils.helpers import get_pair_metadata
 
 async def test_token_derived_eth():
     
-    from_block = 12084850
+    from_block = 12317600
     to_block = from_block + 9
     rpc_helper = RpcHelper(rpc_settings=settings.rpc)
     await rpc_helper.init()
     
-    pair_address = "0xe902EF54E437967c8b37D30E80ff887955c90DB6" # USDbC-WETH
+    pair_address = "0x88A43bbDF9D098eEC7bCEda4e2494615dfD9bB9C" # USDC-WETH
 
     pair_metadata = await get_pair_metadata(
         rpc_helper=rpc_helper,
         pair_address=pair_address
     )
 
-    token_metadata = pair_metadata["token1"] # USDbC
+    token_metadata = pair_metadata["token1"] # USDC
 
     eth_price = await get_token_derived_eth(
         rpc_helper=rpc_helper,
@@ -31,7 +32,17 @@ async def test_token_derived_eth():
         token_metadata=token_metadata
     )
 
-    from pprint import pprint
+    pprint(eth_price)
+
+    token_metadata = pair_metadata["token0"] # WETH
+
+    eth_price = await get_token_derived_eth(
+        rpc_helper=rpc_helper,
+        from_block=from_block,
+        to_block=to_block,
+        token_metadata=token_metadata
+    )
+
     pprint(eth_price)
 
 if __name__ == '__main__':
