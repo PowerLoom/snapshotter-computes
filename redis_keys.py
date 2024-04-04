ad08ba49898d764ae2b7b5cbec843c2c1a4cfd71
Seth-Schmidt <seth@powerloom.io>
2024-04-04 19:55:57 -0400

    chore: clean unused configs, models, and code

--- redis_keys.py
+++ redis_keys.py
@@ -1,39 +1,3 @@
 from snapshotter.settings.config import settings
 
-aave_asset_contract_data = (
-    'aave:assetContract:' + settings.namespace + ':{}:AssetContractData'
-)
 
-aave_cached_block_height_asset_price = (
-    'aave:assetContract:' + settings.namespace +
-    ':{}:cachedAaveBlockHeightAssetPrice'
-)
-
-aave_cached_block_height_asset_data = (
-    'aave:assetContract:' + settings.namespace +
-    ':{}:cachedAaveBlockHeightAssetData'
-)
-
-aave_cached_block_height_asset_details = (
-    'aave:assetContract:' + settings.namespace +
-    ':{}:cachedAaveBlockHeightAssetDetails'
-)
-
-aave_cached_block_height_asset_rate_details = (
-    'aave:assetContract:' + settings.namespace +
-    ':{}:cachedAaveBlockHeightAssetRateDetails'
-)
-
-aave_pool_asset_set_data = (
-    'aave:poolContract:' + settings.namespace + ':assetSetData'
-)
-
-aave_cached_block_height_core_event_data = (
-    'aave:poolContract:' + settings.namespace +
-    ':cachedAaveBlockHeightEventData'
-)
-
-aave_cached_block_height_assets_prices = (
-    'aave:poolContract:' + settings.namespace +
-    ':cachedAaveBlockHeightAssetsPrices'
-)
--- settings/settings_model.py
+++ settings/settings_model.py
@@ -4,43 +4,4 @@ from pydantic import BaseModel
 from pydantic import Field
 
 
-class AaveContractAbis(BaseModel):
-    pool_contract: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/AaveV3Pool.json',
-    )
-    pool_data_provider_contract: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/AaveProtocolDataProvider.json',
-    )
-    erc20: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/IERC20.json',
-    )
-    a_token: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/AToken.json',
-    )
-    stable_token: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/OneInchQuoter.json',
-    )
-    variable_token: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/OneInchQuoter.json',
-    )
-    aave_oracle: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/OneInchQuoter.json',
-    )
-    ui_pool_data_provider: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/UiPoolDataProvider.json',
-    )
 
-
-class ContractAddresses(BaseModel):
-    WETH: str = Field(..., example='0x853d955aCEf822Db058eb8505911ED77F175b99e')
-    MAKER: str = Field(..., example='0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2')
-    aave_v3_pool: str = Field(..., example='0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2')
-    pool_data_provider: str = Field(..., example='0x7B4EB56E7CD4b454BA8ff71E4518426369a138a3')
-    aave_oracle: str = Field(..., example='0x7B4EB56E7CD4b454BA8ff71E4518426369a138a3')
-    ui_pool_data_provider: str = Field(..., example='0x7B4EB56E7CD4b454BA8ff71E4518426369a138a3')
-    pool_address_provider: str = Field(..., example='0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e')
-
-
-class Settings(BaseModel):
-    aave_contract_abis: AaveContractAbis
-    contract_addresses: ContractAddresses
