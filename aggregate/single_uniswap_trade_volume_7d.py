379656b89f129cdd3fb347aa6bf7c1fc6773e83e
Seth-Schmidt <seth@powerloom.io>
2024-04-11 22:49:11 -0400

    chore: merge 7d volume aggregate from eth_uniswapv2 branch

--- aggregate/single_uniswap_trade_volume_7d.py
+++ aggregate/single_uniswap_trade_volume_7d.py
@@ -3,7 +3,9 @@ import asyncio
 import pydantic
 from ipfs_client.main import AsyncIPFSClient
 from redis import asyncio as aioredis
-from snapshotter.utils.callback_helpers import GenericProcessorSingleProjectAggregate
+from ..utils.helpers import truncate
+from ..utils.models.message_models import UniswapTradesAggregateSnapshot
+from snapshotter.utils.callback_helpers import GenericProcessorAggregate
 from snapshotter.utils.data_utils import get_project_epoch_snapshot
 from snapshotter.utils.data_utils import get_submission_data
 from snapshotter.utils.data_utils import get_tail_epoch_id
@@ -11,11 +13,8 @@ from snapshotter.utils.default_logger import logger
 from snapshotter.utils.models.message_models import PowerloomSnapshotSubmittedMessage
 from snapshotter.utils.rpc import RpcHelper
 
-from ..utils.helpers import truncate
-from ..utils.models.message_models import UniswapTradesAggregateSnapshot
 
-
-class AggregateTradeVolumeProcessor(GenericProcessorSingleProjectAggregate):
+class AggregateTradeVolumeProcessor(GenericProcessorAggregate):
     transformation_lambdas = None
 
     def __init__(self) -> None:
@@ -27,6 +26,7 @@ class AggregateTradeVolumeProcessor(GenericProcessorSingleProjectAggregate):
         previous_aggregate_snapshot: UniswapTradesAggregateSnapshot,
         current_snapshot: UniswapTradesAggregateSnapshot,
     ):
+
         previous_aggregate_snapshot.totalTrade += current_snapshot.totalTrade
         previous_aggregate_snapshot.totalFee += current_snapshot.totalFee
         previous_aggregate_snapshot.token0TradeVolume += current_snapshot.token0TradeVolume
@@ -41,6 +41,7 @@ class AggregateTradeVolumeProcessor(GenericProcessorSingleProjectAggregate):
         previous_aggregate_snapshot: UniswapTradesAggregateSnapshot,
         current_snapshot: UniswapTradesAggregateSnapshot,
     ):
+
         previous_aggregate_snapshot.totalTrade -= current_snapshot.totalTrade
         previous_aggregate_snapshot.totalFee -= current_snapshot.totalFee
         previous_aggregate_snapshot.token0TradeVolume -= current_snapshot.token0TradeVolume
@@ -68,10 +69,9 @@ class AggregateTradeVolumeProcessor(GenericProcessorSingleProjectAggregate):
         ipfs_reader: AsyncIPFSClient,
         protocol_state_contract,
         project_id: str,
+
     ):
-        self._logger.info(
-            f'Building 7 day trade volume aggregate snapshot against {msg_obj}',
-        )
+        self._logger.info(f'Building 7 day trade volume aggregate snapshot against {msg_obj}')
 
         contract = project_id.split(':')[-2]
 
@@ -80,39 +80,26 @@ class AggregateTradeVolumeProcessor(GenericProcessorSingleProjectAggregate):
         )
         # 24h snapshots fetches
         snapshot_tasks = list()
-        self._logger.debug(
-            'fetching 24hour aggregates spaced out by 1 day over 7 days...',
-        )
-        # 1. find one day tail epoch
+        self._logger.debug('fetching 24hour aggregates spaced out by 1 day over 7 days...')
         count = 1
         self._logger.debug(
             'fetch # {}: queueing task for 24h aggregate snapshot for project ID {}'
             ' at currently received epoch ID {} with snasphot CID {}',
-            count,
-            msg_obj.projectId,
-            msg_obj.epochId,
-            msg_obj.snapshotCid,
+            count, msg_obj.projectId, msg_obj.epochId, msg_obj.snapshotCid,
         )
 
         snapshot_tasks.append(
             get_submission_data(
-                redis,
-                msg_obj.snapshotCid,
-                ipfs_reader,
-                msg_obj.projectId,
+                redis, msg_obj.snapshotCid, ipfs_reader, msg_obj.projectId,
             ),
         )
+
         seek_stop_flag = False
         head_epoch = msg_obj.epochId
         # 2. if not extrapolated, attempt to seek further back
-        while not seek_stop_flag or count < 7:
+        while not seek_stop_flag and count < 7:
             tail_epoch_id, seek_stop_flag = await get_tail_epoch_id(
-                redis,
-                protocol_state_contract,
-                anchor_rpc_helper,
-                head_epoch,
-                86400,
-                msg_obj.projectId,
+                redis, protocol_state_contract, anchor_rpc_helper, head_epoch, 86400, msg_obj.projectId,
             )
             count += 1
             snapshot_tasks.append(
@@ -127,24 +114,18 @@ class AggregateTradeVolumeProcessor(GenericProcessorSingleProjectAggregate):
         self._logger.debug(
             'for 7d aggregated trade volume calculations: fetched {} '
             '24h aggregated trade volume snapshots for project ID {}: {}',
-            len(all_snapshots),
-            msg_obj.projectId,
-            all_snapshots,
+            len(all_snapshots), msg_obj.projectId, all_snapshots,
         )
         complete_flags = []
         for single_24h_snapshot in all_snapshots:
             if not isinstance(single_24h_snapshot, BaseException):
                 try:
-                    snapshot = UniswapTradesAggregateSnapshot.parse_obj(
-                        single_24h_snapshot,
-                    )
+                    snapshot = UniswapTradesAggregateSnapshot.parse_obj(single_24h_snapshot)
                     complete_flags.append(snapshot.complete)
                 except pydantic.ValidationError:
                     pass
                 else:
-                    aggregate_snapshot = self._add_aggregate_snapshot(
-                        aggregate_snapshot, snapshot,
-                    )
+                    aggregate_snapshot = self._add_aggregate_snapshot(aggregate_snapshot, snapshot)
 
         if not all(complete_flags) or count < 7:
             aggregate_snapshot.complete = False
