67c6de8f2894421858ee3a123197ec73c2b31fc8
Seth-Schmidt <seth@powerloom.io>
2024-04-11 22:59:56 -0400

    chore: merge top pair 24h aggregate from eth_uniswapv2

--- aggregate/multi_uniswap_top_pairs_24h.py
+++ aggregate/multi_uniswap_top_pairs_24h.py
@@ -6,19 +6,19 @@ from ..utils.models.message_models import UniswapPairTotalReservesSnapshot
 from ..utils.models.message_models import UniswapTopPair24hSnapshot
 from ..utils.models.message_models import UniswapTopPairs24hSnapshot
 from ..utils.models.message_models import UniswapTradesAggregateSnapshot
-from pooler.utils.callback_helpers import GenericProcessorMultiProjectAggregate
-from pooler.utils.data_utils import get_sumbmission_data_bulk
-from pooler.utils.default_logger import logger
-from pooler.utils.models.message_models import PowerloomCalculateAggregateMessage
-from pooler.utils.rpc import RpcHelper
+from snapshotter.utils.callback_helpers import GenericProcessorAggregate
+from snapshotter.utils.data_utils import get_submission_data_bulk
+from snapshotter.utils.default_logger import logger
+from snapshotter.utils.models.message_models import PowerloomCalculateAggregateMessage
+from snapshotter.utils.rpc import RpcHelper
 
 
-class AggregateTopPairsProcessor(GenericProcessorMultiProjectAggregate):
+class AggreagateTopPairsProcessor(GenericProcessorAggregate):
     transformation_lambdas = None
 
     def __init__(self) -> None:
         self.transformation_lambdas = []
-        self._logger = logger.bind(module="AggregateTopPairsProcessor")
+        self._logger = logger.bind(module='AggregateTopPairsProcessor')
 
     async def compute(
         self,
@@ -29,33 +29,33 @@ class AggregateTopPairsProcessor(GenericProcessorMultiProjectAggregate):
         ipfs_reader: AsyncIPFSClient,
         protocol_state_contract,
         project_id: str,
+
     ):
-        self._logger.info(
-            f"Calculating 24h top pairs trade volume and reserves data for {msg_obj}"
-        )
+        self._logger.info(f'Calculating 24h top pairs trade volume and reserves data for {msg_obj}')
 
         epoch_id = msg_obj.epochId
 
         snapshot_mapping = {}
         all_pair_metadata = {}
 
-        snapshot_data = await get_sumbmission_data_bulk(
-            redis,
-            [msg.snapshotCid for msg in msg_obj.messages],
-            ipfs_reader,
-            [msg.projectId for msg in msg_obj.messages],
+        snapshot_data = await get_submission_data_bulk(
+            redis, [msg.snapshotCid for msg in msg_obj.messages], ipfs_reader, [
+                msg.projectId for msg in msg_obj.messages
+            ],
         )
 
+        complete_flags = []
         for msg, data in zip(msg_obj.messages, snapshot_data):
             if not data:
                 continue
-            if "reserves" in msg.projectId:
+            if 'reserves' in msg.projectId:
                 snapshot = UniswapPairTotalReservesSnapshot.parse_obj(data)
-            elif "volume" in msg.projectId:
+            elif 'volume' in msg.projectId:
                 snapshot = UniswapTradesAggregateSnapshot.parse_obj(data)
+                complete_flags.append(snapshot.complete)
             snapshot_mapping[msg.projectId] = snapshot
 
-            contract_address = msg.projectId.split(":")[-2]
+            contract_address = msg.projectId.split(':')[-2]
             if contract_address not in all_pair_metadata:
                 pair_metadata = await get_pair_metadata(
                     contract_address,
@@ -69,28 +69,26 @@ class AggregateTopPairsProcessor(GenericProcessorMultiProjectAggregate):
         pair_data = {}
         for snapshot_project_id in snapshot_mapping.keys():
             snapshot = snapshot_mapping[snapshot_project_id]
-            contract = snapshot_project_id.split(":")[-2]
+            contract = snapshot_project_id.split(':')[-2]
             pair_metadata = all_pair_metadata[contract]
 
             if contract not in pair_data:
                 pair_data[contract] = {
-                    "address": contract,
-                    "name": pair_metadata["pair"]["symbol"],
-                    "liquidity": 0,
-                    "volume24h": 0,
-                    "fee24h": 0,
+                    'address': contract,
+                    'name': pair_metadata['pair']['symbol'],
+                    'liquidity': 0,
+                    'volume24h': 0,
+                    'fee24h': 0,
                 }
 
-            if "reserves" in snapshot_project_id:
+            if 'reserves' in snapshot_project_id:
                 max_epoch_block = snapshot.chainHeightRange.end
-                pair_data[contract]["liquidity"] += (
-                    snapshot.token0ReservesUSD[f"block{max_epoch_block}"]
-                    + snapshot.token1ReservesUSD[f"block{max_epoch_block}"]
-                )
+                pair_data[contract]['liquidity'] += snapshot.token0ReservesUSD[f'block{max_epoch_block}'] + \
+                    snapshot.token1ReservesUSD[f'block{max_epoch_block}']
 
-            elif "volume" in snapshot_project_id:
-                pair_data[contract]["volume24h"] += snapshot.totalTrade
-                pair_data[contract]["fee24h"] += snapshot.totalFee
+            elif 'volume' in snapshot_project_id:
+                pair_data[contract]['volume24h'] += snapshot.totalTrade
+                pair_data[contract]['fee24h'] += snapshot.totalFee
 
         top_pairs = []
         for pair in pair_data.values():
@@ -102,5 +100,8 @@ class AggregateTopPairsProcessor(GenericProcessorMultiProjectAggregate):
             epochId=epoch_id,
             pairs=top_pairs,
         )
+
+        if not all(complete_flags):
+            top_pairs_snapshot.complete = False
 
         return top_pairs_snapshot
