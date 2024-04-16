6f5949462ce97cc4b0dfab2858c7cb1a39ad775b
Seth-Schmidt <seth@powerloom.io>
2024-04-15 21:38:51 -0400

    chore: update settings config and model

--- settings/settings_model.py
+++ settings/settings_model.py
@@ -4,4 +4,9 @@ from pydantic import BaseModel
 from pydantic import Field
 
 
+class ContractAbis(BaseModel):
+    erc721: str = Field(..., example='snapshotter/modules/computes/static/abis/ERC721.json')
 
+
+class Settings(BaseModel):
+    contract_abis: ContractAbis
