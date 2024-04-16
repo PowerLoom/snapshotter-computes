ca11b077e6c1516b2d2fda294db3f75e7a941733
Seth-Schmidt <seth@powerloom.io>
2024-04-15 21:40:11 -0400

    chore: update data models for mint data

--- utils/models/data_models.py
+++ utils/models/data_models.py
@@ -4,6 +4,21 @@ from typing import List
 from pydantic import BaseModel
 
 
+class MintData(BaseModel):
+    minter: str
+    tokenId: int
+    transactionHash: str
+
+
+class BlockMintData(BaseModel):
+    mints: List[MintData]
+    timestamp: int
+
+
+class EpochMintData(BaseModel):
+    minted: Dict[int, BlockMintData]
+
+
 class detailsData(BaseModel):
     number: int
     timestamp: int
