09f9f28cbb2db95e27e3f1074b3815f437d5b6af
Seth-Schmidt <seth@powerloom.io>
2024-04-23 22:03:15 -0400

    chore: update data model to include apr

--- utils/models/data_models.py
+++ utils/models/data_models.py
@@ -4,9 +4,10 @@ from typing import List
 from pydantic import BaseModel
 
 
-class EthSharesData(BaseModel):
-    lastTimestamp: int
+class LidoAprData(BaseModel):
+    timeElapsed: int
     preTotalShares: int
     preTotalEther: int
     postTotalShares: int
     postTotalEther: int
+    stakingApr: float = 0.0
