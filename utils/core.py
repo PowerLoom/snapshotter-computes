cd4fbc9ae5e8a41797b39979e09c6c10aab60c4a
Seth-Schmidt <seth@powerloom.io>
2024-04-23 22:08:35 -0400

    chore: remove old apr calculation

--- utils/core.py
+++ utils/core.py
@@ -2,17 +2,12 @@ import json
 
 from redis import asyncio as aioredis
 from snapshotter.utils.default_logger import logger
-from snapshotter.utils.rpc import get_contract_abi_dict
 from snapshotter.utils.rpc import RpcHelper
 from snapshotter.utils.snapshot_utils import (
     get_block_details_in_block_range,
 )
-from web3 import Web3
 
 from ..redis_keys import lido_contract_cached_block_height_shares
-from .constants import lido_contract_abi
-from .constants import lido_contract_object
-from .constants import SECONDS_IN_YEAR
 from .helpers import get_last_token_rebase
 from .models.data_models import EthSharesData
 
@@ -24,7 +19,6 @@ async def get_lido_staking_yield(
     rpc_helper: RpcHelper,
     from_block: int,
     to_block: int,
-    fetch_timestamp=True,
 ):
 
     # get block details for the range in order to get the timestamps
@@ -46,28 +40,6 @@ async def get_lido_staking_yield(
         )
         raise err
 
-    # create dictionary of ABI {function_name -> {signature, abi, input, output}}
-    lido_abi_dict = get_contract_abi_dict(lido_contract_abi)
-    lido_address = lido_contract_object.address
-
-    shares_array = await rpc_helper.batch_eth_call_on_block_range(
-        abi_dict=lido_abi_dict,
-        function_name='getTotalShares',
-        contract_address=lido_address,
-        from_block=from_block,
-        to_block=to_block,
-        redis_conn=redis_conn,
-    )
-
-    eth_array = await rpc_helper.batch_eth_call_on_block_range(
-        abi_dict=lido_abi_dict,
-        function_name='getTotalPooledEther',
-        contract_address=lido_address,
-        from_block=from_block,
-        to_block=to_block,
-        redis_conn=redis_conn,
-    )
-
     # attempt to get the previous epoch shares data, if it exists it will be cached to the to_block
     prev_epoch_shares_data = await redis_conn.hget(
         lido_contract_cached_block_height_shares,
@@ -87,29 +59,6 @@ async def get_lido_staking_yield(
             from_block=from_block,
         )
 
-    # pre_share_rate = last_shares_data.preTotalEther * 1e27 / last_shares_data.preTotalShares
-    # post_share_rate = last_shares_data.postTotalEther * 1e27 / last_shares_data.postTotalShares
-
-    # prev_apr = SECONDS_IN_YEAR * (
-    #     (post_share_rate - pre_share_rate) / pre_share_rate
-    # ) / 86400
-
-    for index, block_num in enumerate(range(from_block, to_block + 1)):
+    for block_num in range(from_block, to_block + 1):
         block_details = block_details_dict.get(block_num, {})
         block_timestamp = block_details.get('timestamp', None)
-        block_shares = shares_array[index][0]
-        block_eth = eth_array[index][0]
-
-        time_elapsed = block_timestamp - last_shares_data.lastTimestamp
-
-        pre_share_rate = last_shares_data.postTotalEther * 1e27 / last_shares_data.postTotalShares
-        post_share_rate = block_eth * 1e27 / block_shares
-
-        annual_rate_change = SECONDS_IN_YEAR * (
-            (post_share_rate - pre_share_rate) / pre_share_rate
-        ) / time_elapsed
-
-        last_shares_data.lastTimestamp = block_timestamp
-        last_shares_data.postTotalShares = block_shares
-        last_shares_data.postTotalEther = block_eth
-        prev_apr = annual_rate_change
