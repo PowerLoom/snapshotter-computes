84f7f1f0e32f208b1075c5d23fc262f4be042ff1
Seth <seth@powerloom.io>
2024-10-14 19:55:39 -0400

    chore: add uniswap pair contract abi for preloader

--- settings/settings_model.py
+++ settings/settings_model.py
@@ -3,26 +3,32 @@ from pydantic import Field
 
 
 class AaveContractAbis(BaseModel):
-    pool_contract: str = Field(...)
-    pool_data_provider_contract: str = Field(...)
-    erc20: str = Field(...)
-    a_token: str = Field(...)
-    stable_token: str = Field(...)
-    variable_token: str = Field(...)
-    aave_oracle: str = Field(...)
-    ui_pool_data_provider: str = Field(...)
+    pool_contract: str
+    pool_data_provider_contract: str
+    erc20: str
+    a_token: str
+    stable_token: str
+    variable_token: str
+    aave_oracle: str
+    ui_pool_data_provider: str
+
+
+# used for ETH price calculation in preloaders/eth_price/preloader.py
+class UniswapContractAbis(BaseModel):
+    pair_contract: str
 
 
 class ContractAddresses(BaseModel):
-    WETH: str = Field(...)
-    MAKER: str = Field(...)
-    aave_v3_pool: str = Field(...)
-    pool_data_provider: str = Field(...)
-    aave_oracle: str = Field(...)
-    ui_pool_data_provider: str = Field(...)
-    pool_address_provider: str = Field(...)
+    WETH: str
+    MAKER: str
+    aave_v3_pool: str
+    pool_data_provider: str
+    aave_oracle: str
+    ui_pool_data_provider: str
+    pool_address_provider: str
 
 
 class Settings(BaseModel):
     aave_contract_abis: AaveContractAbis
     contract_addresses: ContractAddresses
+    uniswap_contract_abis: UniswapContractAbis
