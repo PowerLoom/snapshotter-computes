ad4c83322f7067a55c397a060ed964e690e896e8
Seth <seth@powerloom.io>
2024-10-14 16:04:10 -0400

    chore: update file paths and settings models

--- settings/settings_model.py
+++ settings/settings_model.py
@@ -1,44 +1,26 @@
-from typing import List
-
 from pydantic import BaseModel
 from pydantic import Field
 
 
 class AaveContractAbis(BaseModel):
-    pool_contract: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/AaveV3Pool.json',
-    )
-    pool_data_provider_contract: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/AaveProtocolDataProvider.json',
-    )
-    erc20: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/IERC20.json',
-    )
-    a_token: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/AToken.json',
-    )
-    stable_token: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/OneInchQuoter.json',
-    )
-    variable_token: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/OneInchQuoter.json',
-    )
-    aave_oracle: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/OneInchQuoter.json',
-    )
-    ui_pool_data_provider: str = Field(
-        ..., example='snapshotter/modules/computes/static/abis/UiPoolDataProvider.json',
-    )
+    pool_contract: str = Field(...)
+    pool_data_provider_contract: str = Field(...)
+    erc20: str = Field(...)
+    a_token: str = Field(...)
+    stable_token: str = Field(...)
+    variable_token: str = Field(...)
+    aave_oracle: str = Field(...)
+    ui_pool_data_provider: str = Field(...)
 
 
 class ContractAddresses(BaseModel):
-    WETH: str = Field(..., example='0x853d955aCEf822Db058eb8505911ED77F175b99e')
-    MAKER: str = Field(..., example='0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2')
-    aave_v3_pool: str = Field(..., example='0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2')
-    pool_data_provider: str = Field(..., example='0x7B4EB56E7CD4b454BA8ff71E4518426369a138a3')
-    aave_oracle: str = Field(..., example='0x7B4EB56E7CD4b454BA8ff71E4518426369a138a3')
-    ui_pool_data_provider: str = Field(..., example='0x7B4EB56E7CD4b454BA8ff71E4518426369a138a3')
-    pool_address_provider: str = Field(..., example='0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e')
+    WETH: str = Field(...)
+    MAKER: str = Field(...)
+    aave_v3_pool: str = Field(...)
+    pool_data_provider: str = Field(...)
+    aave_oracle: str = Field(...)
+    ui_pool_data_provider: str = Field(...)
+    pool_address_provider: str = Field(...)
 
 
 class Settings(BaseModel):
