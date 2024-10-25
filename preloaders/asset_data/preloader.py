88154da995cf5d79cacfe33e7ddddf65d77998e3
Seth <seth@powerloom.io>
2024-10-25 19:08:46 -0400

    chore: add asset data and asset volume event preloaders

---
+++ preloaders/asset_data/preloader.py
@@ -0,0 +1,57 @@
+from snapshotter.utils.callback_helpers import GenericPreloader
+from snapshotter.utils.default_logger import logger
+from snapshotter.utils.models.data_models import PreloaderResult
+from snapshotter.utils.models.message_models import EpochBase
+from snapshotter.utils.rpc import RpcHelper
+
+from computes.utils.helpers import get_bulk_asset_data
+
+
+class BulkAssetDataPreloader(GenericPreloader):
+    """
+    A preloader class for fetching bulk asset data for Aave protocol.
+    
+    This class is responsible for retrieving asset reserve data for all blocks
+    in a given range and for all assets in the Aave pool.
+    """
+
+    def __init__(self) -> None:
+        self._logger = logger.bind(module='AaveBulkAssetDataPreloader')
+
+    async def compute(
+            self,
+            epoch: EpochBase,
+            rpc_helper: RpcHelper,
+    ):
+        """
+        Compute and store bulk asset data for the given epoch.
+
+        Args:
+            epoch (EpochBase): The epoch object containing begin and end block heights.
+            redis_conn (aioredis.Redis): Redis connection for caching data.
+            rpc_helper (RpcHelper): Helper object for making RPC calls.
+
+        Raises:
+            Exception: If there's an error during the bulk asset data retrieval process.
+        """
+        min_chain_height = epoch.begin
+        max_chain_height = epoch.end
+        
+        try:
+            # Get asset reserve data for all blocks in range for all assets in the Aave pool
+            result = await get_bulk_asset_data(
+                rpc_helper=rpc_helper,
+                from_block=min_chain_height,
+                to_block=max_chain_height,
+            )
+            return PreloaderResult(
+                keyword='bulk_event',
+                result=result,
+            )
+        except Exception as e:
+            self._logger.error(f'Error in Bulk Asset Data preloader: {e}')
+            raise e
+
+    async def cleanup(self):
+        """Perform any necessary cleanup operations."""
+        pass
---
+++ preloaders/volume_events/preloader.py
@@ -0,0 +1,55 @@
+from computes.utils.helpers import get_pool_supply_events
+from snapshotter.utils.callback_helpers import GenericPreloader
+from snapshotter.utils.default_logger import logger
+from snapshotter.utils.models.message_models import EpochBase
+from snapshotter.utils.rpc import RpcHelper
+
+
+class AaveBulkVolumeEventsPreloader(GenericPreloader):
+    """
+    A preloader class for fetching bulk volume events data for the Aave protocol.
+    
+    This class is responsible for retrieving core Aave supply events for all blocks
+    in a given range and for all assets in the Aave pool.
+    """
+
+    def __init__(self) -> None:
+        self._logger = logger.bind(module='AaveBulkVolumeEventsPreloader')
+
+    async def compute(
+            self,
+            epoch: EpochBase,
+            rpc_helper: RpcHelper,
+    ):
+        """
+        Compute and store bulk volume events data for the given epoch.
+
+        Args:
+            epoch (EpochBase): The epoch object containing begin and end block heights.
+            redis_conn (aioredis.Redis): Redis connection for caching data.
+            rpc_helper (RpcHelper): Helper object for making RPC calls.
+
+        Raises:
+            Exception: If there's an error during the bulk volume events data retrieval process.
+        """
+        min_chain_height = epoch.begin
+        max_chain_height = epoch.end
+        
+        try:
+            # Get core Aave supply events for all blocks in range for all assets in the Aave pool
+            result = await get_pool_supply_events(
+                rpc_helper=rpc_helper,
+                from_block=min_chain_height,
+                to_block=max_chain_height,
+            )
+            return PreloaderResult(
+                keyword='bulk_volume',
+                result=result,
+            )
+        except Exception as e:
+            self._logger.error(f'Error in Bulk Volume Event preloader: {e}')
+            raise e
+
+    async def cleanup(self):
+        """Perform any necessary cleanup operations."""
+        pass
