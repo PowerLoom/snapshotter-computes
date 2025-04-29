f94e1413763aeb8c51e9b1bafd6cd441ef17af6b
Seth <seth@powerloom.io>
2025-04-29 16:21:48 +0530

    chore: refactor imports to use rpc_helper for consistency across modules

--- liquidity_depth.py
+++ liquidity_depth.py
@@ -4,12 +4,12 @@ from typing import Optional
 from typing import Union
 
 from redis import asyncio as aioredis
+from rpc_helper.rpc import RpcHelper
 
 from computes.utils.core import get_liquidity_depth
 from computes.utils.models.message_models import LiquidityDepthSnapshot
 from snapshotter.utils.callback_helpers import SnapshotProcessMessage
 from snapshotter.utils.default_logger import logger
-from snapshotter.utils.rpc import RpcHelper
 
 
 class LiquidityDepthProcessor(SnapshotProcessMessage):
