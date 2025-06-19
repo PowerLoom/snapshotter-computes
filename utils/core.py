d2994854a713e1d60714df9de921896ffe8902f0
Seth <seth@powerloom.io>
2025-06-18 21:18:29 -0400

    fix: update token metadata retrieval in get_pair_trade_volume to use model_dump for consistency

--- utils/core.py
+++ utils/core.py
@@ -717,7 +717,7 @@ async def get_pair_trade_volume(
 
     token0_price_map, token1_price_map = await asyncio.gather(
         get_token_price_in_block_range(
-            token_metadata=pair_per_token_metadata.token0.dict(),
+            token_metadata=pair_per_token_metadata.token0.model_dump(),
             from_block=from_block,
             to_block=to_block,
             redis_conn=redis_conn,
@@ -725,7 +725,7 @@ async def get_pair_trade_volume(
             debug_log=False,
         ),
         get_token_price_in_block_range(
-            token_metadata=pair_per_token_metadata.token1.dict(),
+            token_metadata=pair_per_token_metadata.token1.model_dump(),
             from_block=from_block,
             to_block=to_block,
             redis_conn=redis_conn,
